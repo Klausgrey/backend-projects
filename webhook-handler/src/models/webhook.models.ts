6bb976c6f8e55ffba00c3d90850717646c424ab2
Nicholas <danielnick210@gmail.com>
2026-07-11 12:29:46 +0100

    created the post end point

--- webhook-handler/src/models/webhook.models.ts
+++ webhook-handler/src/models/webhook.models.ts
@@ -1,5 +1,19 @@
-import { prisma } from "../config/db";
+import { prisma } from "../config/db.js";
 
-export const createWebhook = async () => {
-	
-}
+export const createWebhook = async (
+	event: string,
+	payload: object,
+	source: string,
+	verified: boolean,
+) => {
+	return await prisma.webhook.create({
+		data: {
+			event,
+			payload,
+			source,
+			verified,
+		},
+	});
+};
+
+
