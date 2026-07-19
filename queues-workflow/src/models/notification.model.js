15ff94dd79dd358d526b254266bc32477b64ec27
Nicholas <danielnick210@gmail.com>
2026-07-19 08:42:21 +0100

    added the delete and mark as read maps

--- queues-workflow/src/models/notification.model.js
+++ queues-workflow/src/models/notification.model.js
@@ -1,13 +1,25 @@
 const notifications = new Map();
 
-const addNotification = (userId, notifications) => {
-	const existing = notifications.get(userId)
-	if (!existing) notifications.set(userId, [notifications]);
-	else existing.push(notifications)
+const addNotification = (userId, newNotifications) => {
+	const existing = notifications.get(userId);
+	if (!existing) notifications.set(userId, [newNotifications]);
+	else existing.push(newNotifications);
 };
 const getNotifications = (userId) => {
 	const data = notifications.get(userId) || [];
 	return data;
 };
-const markAsRead = (userId, notificationId) => {};
-const deleteNotification = (userId, notificationId) => {};
+const markAsRead = (userId, notificationId) => {
+	const userNotis = notification.get(userId);
+	if (!userNotis) return;
+
+	const target = userNotis.find((n) => n.id === notificationId);
+	if (target) target.read = true;
+};
+const deleteNotification = (userId, notificationId) => {
+	const userNotis = notification.get(userId);
+	if (!userNotis) return;
+
+	const filtered = userNotis.filter((n) => n.id !== notificationId);
+	notification.set(userId, filtered);
+};
