947ac19fb74204cb1ed94f05ef79b0d1e1785456
Nicholas <danielnick210@gmail.com>
2026-07-20 21:40:41 +0100

    added the send-notification controller

--- queues-workflow/src/controllers/notification.controller.js
+++ queues-workflow/src/controllers/notification.controller.js
@@ -1,2 +1,22 @@
 import queue from "../queue/notificationQueue.js";
 import worker from "../worker/notificationWorker.js";
+import {
+	addNotification,
+	deleteNotification,
+	markAsRead,
+	getNotifications,
+} from "../models/notification.model.js";
+
+export const sendNotification = async (req, res) => {
+	const { userId, message } = req.body;
+	queue.add("send-notification", { userId, message });
+	res.status(202).json({ message: "notification queued" });
+};
+export const getNotifications = () => {};
+export const readNotification = () => {};
+export const removeNotification = () => {};
+
+// sendNotification — adds a job to the queue
+// getUserNotifications — gets all notifications for a user
+// readNotification — marks a notification as read
+// removeNotification — deletes a notification
--- queues-workflow/src/models/notification.model.js
+++ queues-workflow/src/models/notification.model.js
@@ -1,25 +1,25 @@
 const notifications = new Map();
 
-const addNotification = (userId, newNotifications) => {
+export const addNotification = (userId, newNotifications) => {
 	const existing = notifications.get(userId);
 	if (!existing) notifications.set(userId, [newNotifications]);
 	else existing.push(newNotifications);
 };
-const getNotifications = (userId) => {
+export const getNotifications = (userId) => {
 	const data = notifications.get(userId) || [];
 	return data;
 };
-const markAsRead = (userId, notificationId) => {
-	const userNotis = notification.get(userId);
+export const markAsRead = (userId, notificationId) => {
+	const userNotis = notifications.get(userId);
 	if (!userNotis) return;
 
 	const target = userNotis.find((n) => n.id === notificationId);
 	if (target) target.read = true;
 };
-const deleteNotification = (userId, notificationId) => {
-	const userNotis = notification.get(userId);
+export const deleteNotification = (userId, notificationId) => {
+	const userNotis = notifications.get(userId);
 	if (!userNotis) return;
 
 	const filtered = userNotis.filter((n) => n.id !== notificationId);
-	notification.set(userId, filtered);
+	notifications.set(userId, filtered);
 };
