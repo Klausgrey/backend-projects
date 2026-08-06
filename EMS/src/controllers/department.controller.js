47781c7f25aa68909c0d96e9a9e559b963e7760a
Nicholas <danielnick210@gmail.com>
2026-08-06 17:15:32 +0100

    created the get all department controller

--- EMS/src/controllers/department.controller.js
+++ EMS/src/controllers/department.controller.js
@@ -1,14 +1,38 @@
 import Department from "../models/department.models.js";
+import { sendSuccess, sendError } from "../utils/response.js";
 
 export async function createDepartment(req, res, next) {
 	try {
 		const { name } = req.body;
-		const data = await Department.create({ name: name });
+		const existing = await Department.findOne({ name });
+		if (existing) return sendError(res, 409, "User already exists");
+		const data = await Department.create({ name });
+		return sendSuccess(res, 201, data, "Department created successfully");
 	} catch (err) {
 		next(err);
 	}
 }
 
+export async function getDepartments(req, res, next) {
+	try {
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || 10;
+		const skip = (page - 1) * limit;
+		const data = await Department.find().skip(skip).limit(limit);
+		const total = await Department.countDocuments();
+		return sendSuccess(
+			res,
+			200,
+			{
+				data,
+				pagination: { page, limit, total, pages: Math.ceil(total / limit) },
+			},
+			"All the departments",
+		);
+	} catch (err) {
+		next(err);
+	}
+}
 // ### Departments
 // | Method | Endpoint | Access | Description |
 // |---|---|---|---|
